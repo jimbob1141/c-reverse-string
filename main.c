ba586a60776bf98db4e8e26e284f4171b322bb34
James Woods <james.woods@neotechnology.com>
2022-10-10 21:24:55 +0100

    Simple program to reverse a string and print values to console.

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ main() {
         // sets the value of index 0 to the value at the end of the original string, stepping backwards through it as i increases
         testStringReversed[i] = testString[LENGTH - i - 1];
         // print the value of the original string at the index that has just been copied
-        printf("%6d", testString[i]);
+        printf("%c", testString[i]);
         // when i == LENGTH - 1 this means we have finished copying the final value, prints newline to separate this text from the next output
         if (i == LENGTH - 1) printf("\n");
 
@@ -24,6 +24,6 @@ main() {
 
     // loop through elements of the reversed string and print them
     for (int i = 0; i != LENGTH; i++){
-        printf("%6d", testStringReversed[i]);
+        printf("%c", testStringReversed[i]);
     }
 }
